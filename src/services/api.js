ccba0943bd14f947b9ffedbbef2ecb0fc2d32530
Sauloleocadio <saulomanshu@gmail.com>
2022-01-16 21:52:57 -0300

    fix: Add key for provisionalDomain

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const { REACT_APP_API_URL, REACT_APP_API_KEY } = process.env;
+const { REACT_APP_API_URL, REACT_APP_API_KEY_PROVISIONAL } = process.env;
 
 const baseURL = REACT_APP_API_URL;
-const apiKey = REACT_APP_API_KEY;
+const apiKey = REACT_APP_API_KEY_PROVISIONAL;
 
 const api = axios.create({
   baseURL,
